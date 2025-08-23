3293387318860b22c4ad5f491a15b7de9a887a22
salikhP <pernebek.sm@gmail.com>
2025-08-23 17:38:34 +0500

    style: add new lines

--- src/core/config.py
+++ src/core/config.py
@@ -7,6 +7,7 @@ class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api/v1"
 
@@ -18,9 +19,11 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 5
     max_overflow: int = 10
 
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+
 
 settings = Settings()
--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
 
+
 app = FastAPI()
 app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
+
 
 if __name__ == "__main__":
     uvicorn.run(
